6eda950588c99656a6efad5cedc595a4d31e2bfb
Yingfu Wang <zcemyw4@ucl.ac.uk>
2023-11-03 10:37:59 +0000

    progress

--- src/starter_code/problem1.py
+++ src/starter_code/problem1.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import pandas as pd
 
 def create_dataframe(csv_file):
     """ Creates, prints and returns a pandas dataframe containing data from a csv file
@@ -13,18 +13,19 @@ def create_dataframe(csv_file):
     """
     # 1. Complete the code to create a variable called 'df' that is a pandas dataframe that reads its contents from the
     # csv file.
-    # Use pandas.read_csv https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html
-
+    # Use pandas.read_csv
+    df = pd.read_csv(csv_file)
     # Uncomment the 2 lines of code below to set the pandas display options to show all rows and columns of the dataset
     # Otherwise, by default pandas will only print as many columns and rows as can be displayed in your terminal window
 
     # pd.set_option('display.max_rows', df.shape[0] + 1)
     # pd.set_option('display.max_columns', df.shape[1] + 1)
-
+    pd.set_option('display.max_rows', df.shape[0] + 1)
+    pd.set_option('display.max_columns', df.shape[1] + 1)
     # 2. Add a line of code to using Python print() to print the dataframe contents
-
+    print(df)
     # 4. Complete the line of code to return the dataframe
-    return
+    return df
 
 
 if __name__ == '__main__':
